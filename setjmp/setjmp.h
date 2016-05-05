1f23f998a9fe4ba92243b09f5178c312076248db
Pip <pipcet@gmail.com>
2016-05-05 14:31:51 +0000

    update setjmp, longjmp

--- setjmp/setjmp.h
+++ setjmp/setjmp.h
@@ -111,4 +111,14 @@ extern void siglongjmp (sigjmp_buf __env, int __val)
 
 __END_DECLS
 
+#undef _setjmp
+#undef setjmp
+
+extern int __sigsetjmp_fp (struct __jmp_buf_tag __env[1], int __savemask,
+                           void *fp) __THROWNL;
+
+#define __sigsetjmp(env, savemask) __sigsetjmp_fp((env), (savemask), __builtin_frame_address(0))
+#define setjmp(env) sigsetjmp((env), 0)
+#define _setjmp(env) setjmp(env)
+
 #endif /* setjmp.h  */
