75737fc1c6b2b4a28aceb07784a7abd589d576b7
Pip <pipcet@gmail.com>
2016-06-17 12:46:24 +0000

    implement setjmp

--- sysdeps/wasm/setjmp.c
+++ sysdeps/wasm/setjmp.c
@@ -7,8 +7,9 @@ int __sigsetjmp_fp (struct __jmp_buf_tag env[1], int __savemask,
   void *rpc = NULL;
   void *sp = NULL;
 
-/*   asm volatile("%O0 = rpc|0;" : "=r" (rpc));
-  asm volatile("%O0 = fp+%/|0;" : "=r" (sp)); */
+  asm volatile("(%S0 (get_local $rpc))" : "=r" (rpc));
+  asm volatile("(%S0 (i32.add (get_local $fp) (i32.const %/)))"
+               : "=r" (sp));
 
   env->__jmpbuf->__pc = rpc;
   env->__jmpbuf->__fp = __fp;
