951040050a79d8298b5e7d47bcc11dbd181ea2fd
Pip Cet <pipcet@gmail.com>
2016-11-28 17:34:55 +0000

    cp setjmp to sigsetjmp

--- setjmp/setjmp.h
+++ setjmp/setjmp.h
@@ -117,7 +117,7 @@ __END_DECLS
 extern int __sigsetjmp_fp (struct __jmp_buf_tag __env[1], int __savemask,
                            void *fp) __THROWNL;
 
-#define __sigsetjmp(env, savemask) __sigsetjmp_fp((env), (savemask), __builtin_frame_address(0))
+//#define __sigsetjmp(env, savemask) __sigsetjmp_fp((env), (savemask), __builtin_frame_address(0))
 //#define setjmp(env) sigsetjmp((env), 0)
 //#define _setjmp(env) setjmp(env)
 
--- sysdeps/wasm32/setjmp.c
+++ sysdeps/wasm32/setjmp.c
@@ -30,3 +30,99 @@ int __sigsetjmp_fp (struct __jmp_buf_tag env[1], int __savemask,
 
   return 0;
 }
+
+int __sigsetjmp (jmp_buf env, int savemask)
+{
+  /* TODO get rid of this .flush. */
+  asm volatile(".flush");
+
+  fprintf(stderr, "env %p\n", env);
+
+  void *x;
+
+  asm volatile("%S0\n\t"
+               "get_local $sp1\n\t"
+               "i32.const -16\n\t"
+               "i32.add\n\t"
+               "%R0\n\t"
+               : "=r" (x));
+
+
+  fprintf(stderr, "x0 %p\n", x);
+
+  x = (void *)*(void **)x;
+  x = (void *)*(void **)x;
+
+  fprintf(stderr, "x1 %p\n", x);
+
+  fprintf(stderr, "pc0 %p\n", *(void **)(x+8));
+  fprintf(stderr, "dpc %p\n", *(void **)(x+12));
+
+  asm volatile(
+               //"get_local $sp1\n\t"
+               //"i32.const -16\n\t"
+               //"i32.add\n\t"
+               //"i32.load a=2 0\n\t"
+               //"i32.load a=2 0\n\t"
+               //"i32.load a=2 8\n\t"
+               //"get_local $rpc\n\t"
+               //"i32.store a=2 0\n\t"
+
+               "%1\n\t"
+               "get_local $sp1\n\t"
+               "i32.const -16\n\t"
+               "i32.add\n\t"
+               "i32.load a=2 0\n\t"
+               "i32.load a=2 0\n\t"
+               "i32.load a=2 8\n\t"
+               "i32.store a=2 0\n\t"
+
+               "%1\n\t"
+               "get_local $sp1\n\t"
+               "i32.const -16\n\t"
+               "i32.add\n\t"
+               "i32.load a=2 0\n\t"
+               "i32.load a=2 0\n\t"
+               "i32.load a=2 16\n\t"
+               "i32.store a=2 4\n\t"
+
+               "%1\n\t"
+               "get_local $sp1\n\t"
+               "i32.const -16\n\t"
+               "i32.add\n\t"
+               "i32.load a=2 0\n\t"
+               "i32.load a=2 0\n\t"
+               "i32.store a=2 8\n\t"
+
+               "%1\n\t"
+               "get_local $sp1\n\t"
+               "i32.const -16\n\t"
+               "i32.add\n\t"
+               "i32.store a=2 12\n\t"
+
+               "i32.const 0\n\t"
+               "set_local $r1\n\t"
+               ".dpc .LI7\n\t"
+               "set_local $dpc\n\t"
+               "i32.const 0\n\t"
+               "get_local $sp\n\t"
+               "%1\n\t"
+               "get_local $r1\n\t"
+               "get_local $rpc\n\t"
+               "get_local $sp1\n\t"
+               "i32.const -16\n\t"
+               "i32.add\n\t"
+               "i32.load a=2 0\n\t"
+               "i32.load a=2 0\n\t"
+               "call __sigjmp_save\n\t"
+               "set_local $rp\n\t"
+               "i32.const 3\n\t"
+               "get_local $rp\n\t"
+               "i32.and\n\t"
+               "if[]\n\t"
+               "throw1\n\t"
+               "end\n\t"
+               ".wasmtextlabeldpcdef .LI7\n\t"
+               : : "r" (0), "r" (env));
+  return 0;
+}
