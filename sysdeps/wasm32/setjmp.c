19085d4887dd88a09c7b081b4969edb835b4d1fc
Pip Cet <pipcet@gmail.com>
2020-06-10 19:09:26 +0000

    adjust to new opcodes

--- sysdeps/wasm32/setjmp.c
+++ sysdeps/wasm32/setjmp.c
@@ -15,8 +15,8 @@ int __sigsetjmp_fp (struct __jmp_buf_tag env[1], int __savemask,
   asm volatile(".flush");
 
   asm volatile("%S0\n\t.flush\n\t%1\n\ti32.const 8\n\ti32.add\n\ti32.load a=2 0\n\t%R0" : "=r" (pc0) : "r" (__fp));
-  asm volatile("%S0\n\tget_local $rpc\n\t%R0" : "=r" (rpc));
-  asm volatile("%S0\n\tget_local $fp\n\ti32.const %/\n\ti32.add\n\t%R0"
+  asm volatile("%S0\n\tlocal.get $rpc\n\t%R0" : "=r" (rpc));
+  asm volatile("%S0\n\tlocal.get $fp\n\ti32.const %/\n\ti32.add\n\t%R0"
                : "=r" (sp));
 
   env->__jmpbuf->__pc0 = rpc;
@@ -37,7 +37,7 @@ __libc_sigsetjmp (jmp_buf env, int savemask)
   void *x;
 
   asm volatile("%S0\n\t"
-               "get_local $sp1\n\t"
+               "local.get $sp1\n\t"
                "i32.const -16\n\t"
                "i32.add\n\t"
                "%R0\n\t"
@@ -48,17 +48,17 @@ __libc_sigsetjmp (jmp_buf env, int savemask)
   x = (void *)*(void **)x;
 
   asm volatile(
-               //"get_local $sp1\n\t"
+               //"local.get $sp1\n\t"
                //"i32.const -16\n\t"
                //"i32.add\n\t"
                //"i32.load a=2 0\n\t"
                //"i32.load a=2 0\n\t"
                //"i32.load a=2 8\n\t"
-               //"get_local $rpc\n\t"
+               //"local.get $rpc\n\t"
                //"i32.store a=2 0\n\t"
 
                "%1\n\t"
-               "get_local $sp1\n\t"
+               "local.get $sp1\n\t"
                "i32.const -16\n\t"
                "i32.add\n\t"
                "i32.load a=2 0\n\t"
@@ -67,7 +67,7 @@ __libc_sigsetjmp (jmp_buf env, int savemask)
                "i32.store a=2 0\n\t"
 
                "%1\n\t"
-               "get_local $sp1\n\t"
+               "local.get $sp1\n\t"
                "i32.const -16\n\t"
                "i32.add\n\t"
                "i32.load a=2 0\n\t"
@@ -76,7 +76,7 @@ __libc_sigsetjmp (jmp_buf env, int savemask)
                "i32.store a=2 4\n\t"
 
                "%1\n\t"
-               "get_local $sp1\n\t"
+               "local.get $sp1\n\t"
                "i32.const -16\n\t"
                "i32.add\n\t"
                "i32.load a=2 0\n\t"
@@ -84,29 +84,29 @@ __libc_sigsetjmp (jmp_buf env, int savemask)
                "i32.store a=2 8\n\t"
 
                "%1\n\t"
-               "get_local $sp1\n\t"
+               "local.get $sp1\n\t"
                "i32.const -16\n\t"
                "i32.add\n\t"
                "i32.store a=2 12\n\t"
 
                "i32.const 0\n\t"
-               "set_local $r1\n\t"
+               "local.set $r1\n\t"
                ".dpc .LI7\n\t"
-               "set_local $dpc\n\t"
+               "local.set $dpc\n\t"
                "i32.const -1\n\t"
-               "get_local $sp\n\t"
+               "local.get $sp\n\t"
                "%1\n\t"
-               "get_local $r1\n\t"
-               "get_local $rpc\n\t"
-               "get_local $sp1\n\t"
+               "local.get $r1\n\t"
+               "local.get $rpc\n\t"
+               "local.get $sp1\n\t"
                "i32.const -16\n\t"
                "i32.add\n\t"
                "i32.load a=2 0\n\t"
                "i32.load a=2 0\n\t"
                "call __sigjmp_save\n\t"
-               "set_local $rp\n\t"
+               "local.set $rp\n\t"
                "i32.const 3\n\t"
-               "get_local $rp\n\t"
+               "local.get $rp\n\t"
                "i32.and\n\t"
                "if[]\n\t"
                "throw\n\t"
