21c980e5d6bb21f885be4d24f03da0ed78874215
Pip Cet <pipcet@gmail.com>
2020-07-01 18:24:24 +0000

    csu/elf-init.c: fix __libc_csu_multifile_fini

--- csu/elf-init.c
+++ csu/elf-init.c
@@ -98,16 +98,23 @@ __libc_csu_init_multifile (int argc, char **argv, char **envp)
 void
 __libc_csu_fini_multifile (void)
 {
-  struct multifile_header *mfh = (struct multifile_header *)16384;
-  while (mfh->terminator) {
-    void (**array_start)(void) = (void (**)(void))(long)mfh->fini_array_start;
-    void (**array_end)(void) = (void (**)(void))(long)mfh->fini_array_end;
-    while (array_start < array_end) {
-      array_end--;
-      (**array_end)();
+  struct multifile_header *next = NULL;
+  while (1)
+    {
+      struct multifile_header *mfh = (struct multifile_header *)16384;
+      while ((struct multifile_header *)(long)(mfh->terminator) != next)
+	mfh = (struct multifile_header *)(long)(mfh->terminator);
+      void (**array_start)(void) = (void (**)(void))(long)mfh->fini_array_start;
+      void (**array_end)(void) = (void (**)(void))(long)mfh->fini_array_end;
+      while (array_start < array_end)
+	{
+	  array_end--;
+	  (**array_end)();
+	}
+      if (mfh == (struct multifile_header *)16384)
+	break;
+      next = mfh;
     }
-    mfh = (struct multifile_header *)(long)(mfh->terminator);
-  }
 }
 #endif
 
