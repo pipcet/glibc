883d6e3bad40c683a21fc741f3f8fd8441924930
Pip Cet <pipcet@gmail.com>
2021-01-21 09:12:48 +0000

    sysdeps/wasm32/fstatat.c: add

---
+++ sysdeps/wasm32/fstatat.c
@@ -0,0 +1,36 @@
+/* Copyright (C) 2005-2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+#include <errno.h>
+
+#include "thinthin.h"
+
+int
+__fstatat (int fd, const char *file, struct stat *buf, int flag)
+{
+  if (buf == NULL)
+    {
+      __set_errno (EINVAL);
+      return -1;
+    }
+
+  return __THINTHIN_SYSCALL (newfstatat, fd, file, buf, flag);
+}
+
+weak_alias (__fstatat, fstatat)
